b372236b004cc5ef84fb390d119e5a3034d304a5
开干！ <39855954+jane77u@users.noreply.github.com>
2023-06-05 16:55:25 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 ###export###
 export PATH
 export FRPS_VER=0.45.0
-export FRPS_INIT="https://raw.githubusercontent.com/MvsCode/frps-onekey/master/frps.init"
+export FRPS_INIT="https://raw.githubusercontent.com/jane77u/frps-onekey/main/frps.init"
 export aliyun_download_url="https://code.aliyun.com/MvsCode/frps-onekey/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
 #======================================================================
