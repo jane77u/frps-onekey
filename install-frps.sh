c44a773428df68bc57ae8f939b1d375f523f5e78
开干！ <39855954+jane77u@users.noreply.github.com>
2023-06-05 17:08:59 +0800

    Update install-frps.sh

--- install-frps.sh
+++ install-frps.sh
@@ -18,7 +18,7 @@ str_program_dir="/usr/local/${program_name}"
 program_init="/etc/init.d/${program_name}"
 program_config_file="frps.ini"
 ver_file="/tmp/.frp_ver.sh"
-str_install_shell="https://raw.githubusercontent.com/MvsCode/frps-onekey/master/install-frps.sh"
+str_install_shell="https://raw.githubusercontent.com/jane77u/frps-onekey/main/install-frps.sh"
 shell_update(){
     fun_clangcn "clear"
     echo "Check updates for shell..."
